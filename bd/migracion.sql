bfc7a3481106f4c16f904f53bc8be96c084b8988
Juan Martin Vallejos <tec.juanvallejos@gmail.com>
2020-02-17 01:08:27 -0300

    borrar actualizacion base

--- bd/migracion.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `Personas` ADD `Edad` longtext CHARACTER SET utf8mb4 NULL;
-
-INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
-VALUES ('20200217040122_edad', '3.0.0');
